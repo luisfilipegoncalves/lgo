e2f30d74958fe796a614ed8b7d353f53f3effe1f
Luis Gonçalves <exnlesi@gmail.com>
2020-10-28 22:18:43 +0000

    add main

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+import Intro from "../components/intro";
 import Profile from "../components/profile";
+
+const MainLayout = styled.div`
+  /* display: flex;
+  flex-direction: row; */
+  /* flex-wrap: wrap; */
+
+  .profile {
+    //min-width: 12rem;
+  }
+
+  .intro {
+    
+  }
+`;
 
 export default function Home() {
   return (
-    <div>
+    <MainLayout>
       <Profile />
-    </div>
+      <Intro />
+    </MainLayout>
   );
 }
