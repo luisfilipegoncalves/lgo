1f1410990788fc132c1fe6e00fd7b0a2020200d4
Luis Gonçalves <exnlesi@gmail.com>
2020-10-31 11:50:07 +0000

    improve styling in companies

--- components/trusted-section/organizations.tsx
+++ components/trusted-section/organizations.tsx
@@ -3,69 +3,47 @@ import Image from "next/image";
 import styled from "styled-components";
 
 const OrganizationsStyle = styled.div`
+  --square: 130px;
+
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-column-gap: 3rem;
-  grid-row-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(var(--square), 1fr));
+  grid-column-gap: 30px;
   align-items: center;
 
-  @media only screen and (max-width: 600px) {
-    grid-template-columns: 1fr 1fr;
-    grid-column-gap: 1rem;
-    grid-row-gap: 2.5rem;
-  }
+  > div {
+    width: var(--square);
+    height: calc(var(--square) / 2);
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  img {
-    min-height: 1rem;
-  }
-
-  .euronext {
-    min-height: 2.5rem;
-  }
-
-  .vicomtech {
-    min-height: 1.5rem;
-  }
-
-  .edigma {
-    min-height: 1.25rem;
+    img {
+      width: 100%;
+      height: 100%;
+    }
   }
 `;
 
 const organizations = [
   {
-    src: "/organizations/euronext.svg",
-    alt: "Euronext",
-    width: 160,
-    height: 60,
-    className: "euronext",
-  },
-  {
     src: "/organizations/farfetch.svg",
     alt: "Farfetch",
-    width: 180,
-    height: 60,
   },
   {
     src: "/organizations/taikai.svg",
     alt: "Taikai",
-    width: 140,
-    height: 60,
-    className: "taikai"
   },
   {
-    src: "/organizations/vicomtech.svg",
-    alt: "VicomTech",
-    width: 160,
-    height: 60,
-    className: "vicomtech",
+    src: "/organizations/euronext.svg",
+    alt: "Euronext",
   },
   {
     src: "/organizations/edigma.svg",
     alt: "Edigma",
-    width: 140,
-    height: 60,
-    className: "edigma",
+  },
+  {
+    src: "/organizations/vicomtech.svg",
+    alt: "VicomTech",
   },
 ];
 
@@ -73,13 +51,7 @@ const Organizations = () => {
   return (
     <OrganizationsStyle>
       {organizations.map((org, index) => (
-        <Image
-          key={index}
-          src={org.src}
-          alt={org.alt}
-          className={org.className}
-          unsized
-        />
+        <Image key={index} src={org.src} alt={org.alt} unsized />
       ))}
     </OrganizationsStyle>
   );
