97d2fa589d3816f90cf7a3de981f957b4e05e479
Luis Gonçalves <exnlesi@gmail.com>
2020-11-05 23:39:42 +0000

    add working poc with styling

--- pages/urldecoder.tsx
+++ pages/urldecoder.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Box, Grommet, TextArea } from "grommet";
 import Header from "../components/common/header";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  margin: 2rem;
+  div {
+    margin-top: 3rem;
+  }
+`;
 
 const UrlDecoderPage = () => {
   const [value, setValue] = React.useState("");
@@ -12,32 +20,34 @@ const UrlDecoderPage = () => {
         global: {
           colors: { border: "var(--white)" },
           focus: { border: { color: "var(--highlight)" } },
-          elevation: { light: { xlarge: "0px 0px 16px var(--darkgrey)" } },
+          elevation: { light: { medium: "0px 0px 8px var(--darkgrey)" } },
         },
       }}
     >
-      <div>
+      <Wrapper>
         <Header value={"URL Decoder"} />
-        <Box align="center" elevation={"xlarge"}>
+        <Box align="center" elevation={"medium"}>
           <TextArea
             placeholder="Type or paste your URL encoded input here..."
+            a11yTitle="textared to paste encoded url"
             value={value}
+            resize={"vertical"}
             onChange={(event) => {
               setValue(event.target.value);
               setUrlDecoded(decodeURIComponent(event.target.value));
             }}
           />
         </Box>
-        <br />
-        <br />
-        <Box align="center" gap={"large"} elevation={"xlarge"}>
+        <Box align="center" gap={"large"} elevation={"medium"}>
           <TextArea
             placeholder="URL decoded output goes here..."
+            a11yTitle="textarea for decoded url"
             value={urlDecoded}
+            resize={"vertical"}
             readOnly
           />
         </Box>
-      </div>
+      </Wrapper>
     </Grommet>
   );
 };
